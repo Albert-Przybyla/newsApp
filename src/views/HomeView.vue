7449c1e495d9cc99d80182e76a7b02f6d927e8ba
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2022-08-05 15:55:21 +0200

    create the basis of the project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'App',
+        date() {
+        return {
+            searchValue: '',
+        }
+    },
+    Methods: {
+        handleInput(){
+            console.log(searchValue)
+        }
+    }
+}
+
+</script>
