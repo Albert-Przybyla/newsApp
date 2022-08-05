c70d731c95c9ce2b79d73597c4f14032f076e05a
APrzybyla <106275293+Albert-Przybyla@users.noreply.github.com>
2022-08-05 15:50:57 +0200

    create the basis of the project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+ import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">news</RouterLink>
+        <RouterLink to="/search">search</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      title: "dsdas"
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+
+
+@media (min-width: 1024px) {
+
+}
+</style>
